5548a2c08cdcd5defad291b64aea442f612b2996
Eduardo Oris <orisdevgg@gmail.com>
2022-09-23 05:12:53 -0300

    Initial commit

---
+++ Aula12/ex013.js
@@ -0,0 +1,26 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+
+switch(diaSem) {
+    case 0: 
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+    case 4:
+        console.log('Quinta')
+    case 5:
+        console.log('Sexta')
+    case 6:
+        console.log('Sábado')
+        default:
+            console.log('[ERRO]')
+            break
+}
